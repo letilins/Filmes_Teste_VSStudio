c567b7e86190fad3e534c35c49b26a4bf6d02b6e
Letícia Lins <leticialins.rj@gmail.com>
2023-10-24 01:13:42 -0300

    first commit

---
+++ Controllers/Filmecontroller.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq; 
+using Filmes_Teste.Models; 
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilmeController : ControllerBase
+{
+    private List<Filme> Filmes = new List<Filme>();
+
+    // GET
+    [HttpGet("{id}", Name = "Get")]
+    public ActionResult<Filme> Get(int id)
+    {
+        var movie = Filmes.FirstOrDefault(m => m.Id == id);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+        return movie;
+    }
+
+    // PUT
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, Filme filme)
+    {
+        var existingMovie = Filmes.FirstOrDefault(m => m.Id == id);
+        if (existingMovie == null)
+        {
+            return NotFound();
+        }
+        existingMovie.Nome = filme.Nome;
+        existingMovie.DataLancamento = filme.DataLancamento; // Corrija o nome da propriedade.
+        return NoContent();
+    }
+
+    // DELETE
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var movie = Filmes.FirstOrDefault(m => m.Id == id);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+        Filmes.Remove(movie);
+        return NoContent();
+    }
+}
---
+++ Models/Filmes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Filmes_Teste.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataLancamento { get; set; }
+    }
+}
+
